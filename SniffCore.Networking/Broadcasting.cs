f7060d77d5acc2850a5deb0536307b0aad846b32
David Wendland <the.devicenator@gmail.com>
2021-06-09 16:42:55 +0200

    Move internal objects to own namespace

--- SniffCore.Networking/Broadcasting.cs
+++ SniffCore.Networking/Broadcasting.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Collections.Generic;
+using SniffCore.Networking.Internal;
 
 namespace SniffCore.Networking
 {
--- SniffCore.Networking/Internal/BroadcastClient.cs
+++ SniffCore.Networking/Internal/BroadcastClient.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 
-namespace SniffCore.Networking
+namespace SniffCore.Networking.Internal
 {
     internal sealed class BroadcastClient
     {
